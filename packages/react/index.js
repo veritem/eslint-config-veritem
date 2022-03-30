37c01bf51fc40ecbd5daeb4c8d9f42658f36ff25
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-03-30 22:14:44 +0200

    feat: updated versioning

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,3 +1,13 @@
 module.exports = {
-
+    extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:import/react',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        }
+    }
 }
