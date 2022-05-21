1273add2df1e633532205b57e52aeb85d2f2983d
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-21 22:21:41 +0200

    feat: updated

--- packages/base/index.js
+++ packages/base/index.js
@@ -1,9 +1,59 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "never"],
-    }
-}
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".mjs"],
+      },
+    },
+  },
+  ignorePatterns: [
+    "*.min.*",
+    "CHANGELOG.md",
+    "dist",
+    "LICENSE*",
+    "output",
+    "coverage",
+    "public",
+    "temp",
+    "packages-lock.json",
+    "pnpm-lock.yaml",
+    "yarn.lock",
+    "__snapshots__",
+    "!.github",
+    "!.vitepress",
+    "!.vscode",
+  ],
+  extends: [
+    "eslint:recommended",
+    "pluginplugin:import/recommended",
+    "plugin:eslint-comments/recommended",
+  ],
+  rules: {
+    // import
+    "import/order": "error",
+    "import/first": "error",
+    "import/no-mutable-exports": "error",
+    "import/no-unresolved": "off",
+    "import/no-absolute-path": "off",
+
+    // common
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    curly: ["error", "multi-or-nest", "consistent"],
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+  },
+};
